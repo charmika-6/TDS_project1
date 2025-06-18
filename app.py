d9cdc75c47037c043fc1775cc3eb300a8781f59e
charmika-6 <23f2004901@ds.study.iitm.ac.in>
2025-06-18 15:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -726,5 +726,5 @@ async def health_check():
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True) 
+#if __name__ == "__main__":
+ #   uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True) 
